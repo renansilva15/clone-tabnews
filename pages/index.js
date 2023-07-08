fdc23594480f1a8ac9a6fea117beb5dbbed3adf5
renansilva15 <renangabrielsilva150@gmail.com>
2023-07-08 11:32:13 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <h1>
+      Miau miau miau miau!<br />
+      Digo,<br />
+      Uma frase para uma gatinha!<br />
+      ᓚᘏᗢ
+    </h1>
+  );
+}
+
+
+export default Home;
